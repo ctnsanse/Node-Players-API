f8bb1571bccb3ea4dc21aacd88c693cb5ecd558d
Ctnsanse <cleytonsanse@gmail.com>
2023-11-28 13:51:07 +0100

    + delete

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express")
 
 const players = require("./mock-players")
 
+const player = require("./mock-players")
+
 const app = express()
 app.use(express.json())
 const port = 3000
@@ -56,8 +58,13 @@ app.put("/api/players/:id", (req, res) => {
     // Hint: return res.end()
 
     // Some pour vérifier si au moins un joueur dans le tableau "players" a le même ID que celui fourni dans la requête
-    const playerExists = players.some(player => player.id === id);
-
+    let playerExists = false
+    for (let index = 0; index < players.length; index++) {
+        if (players[index].id === playerToCreate.id) {
+            playerExists = true
+            break
+        }
+    }
     if (!playerExists) {
         return res.status(404).json({ error: "Player does not exist" });
     }
@@ -69,23 +76,25 @@ app.put("/api/players/:id", (req, res) => {
     return (` You have just modified ${playerUpdate} !`)
 })
 
-app.delete("/api/players/:id", (req, res) => {
+app.delete("/api/players/:id", async (req, res) => {
     // TODO Add coment and implementation
+    const playerToDelete = req.body
+    console.log(playerToDelete)
     const id = req.params.id * 1
 
-    //TODO Check if player with id exists
-    // if not return status code 404 Not Found
+    // TODO Check if a player with the same name exists
     const playerExists = players.some(player => player.id === id)
 
+    // "!" signifie que si l'instruction sera executée que si il y a false.
     if (!playerExists) {
-        return res.status(404).json({ error: "Player does not exist" })
+        return res.status(409).json({ error: "Player no exists" })
     }
 
     //TODO delete a players
-    const playerToDelete = players.delete(playerExists)
+    delete players[id]
 
     //TODO return deleted player id and status code
-    res.status(200).json(` successfuly : Vous venez de supprimer ${players.name} de votre base de données !`)
+    res.status(200).json(` successfuly : Vous venez de supprimer ${player.name} de votre base de données !`)
 })
 
 app.listen(port, () => console.log(`Our Node application is started on : http://localhost:${port}`))
